232f12f3718eeb2c8e3dcd7bdba1b54bb37290c6
gondiaz <gonzalodiazlopez10@gmail.com>
2021-05-25 22:05:35 +0200

    Add lt creation nb

--- nexus_job_templates/lt_production/LT_Template.sh
+++ nexus_job_templates/lt_production/LT_Template.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --time 18:00:00          # Maximum execution time (HH:MM:SS)
+#SBATCH --time 24:00:00          # Maximum execution time (HH:MM:SS)
 #SBATCH --job-name LT
 #SBATCH -o /mnt/lustre/scratch/home/usc/ie/gdl/NEXUS/logs/%A_%a.out        # Standard output
 #SBATCH -e /mnt/lustre/scratch/home/usc/ie/gdl/NEXUS/logs/%A_%a.err        # Standard error
-##SBATCH --qos shared_short
-##SBATCH --partition shared
+#SBATCH --qos shared_short
+#SBATCH --partition shared
 ##SBATCH --qos amd-shared
 ##SBATCH --partition amd-shared
-#SBATCH --qos cl-intel-shared
-#SBATCH --partition cl-intel-shared
+##SBATCH --qos cl-intel-shared
+##SBATCH --partition cl-intel-shared
 #SBATCH -n 1
 #SBATCH -N 1
 
@@ -23,7 +23,7 @@ X="{X}"
 Y="{Y}"
 Z="{Z}"
 NODEOUTDIR="$LUSTRE_SCRATCH/$SLURM_ARRAY_JOB_ID/$SLURM_ARRAY_TASK_ID/"
-OUTFILE="$NODEOUTDIR/nexus_x_{X}_y_{Y}_z_{Z}_next100LT"
+OUTFILE="$NODEOUTDIR/nexus_x_{X}_y_{Y}_z_{Z}_LT"
 
 OUTDIR="$LUSTRE/NEXUS/LT_generation/NEXT100/$Z/prod/"
 mkdir -p $NODEOUTDIR $OUTDIR
